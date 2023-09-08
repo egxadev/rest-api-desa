4757b5637dde2a40a9b09127801c5515a1ee9255
Ega Alifya Firnando <egaalifyaf@gmail.com>
2023-09-09 00:15:19 +0700

    create: resful api public

--- routes/api.php
+++ routes/api.php
@@ -2,16 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
 
 //route login
 Route::post('/login', [App\Http\Controllers\Api\Auth\LoginController::class, 'index']);
@@ -23,11 +13,11 @@ Route::group(['middleware' => 'auth:api'], function () {
     Route::post('/logout', [App\Http\Controllers\Api\Auth\LoginController::class, 'logout']);
 });
 
-
 //group route with prefix "admin"
 Route::prefix('admin')->group(function () {
     //group route with middleware "auth:api"
     Route::group(['middleware' => 'auth:api'], function () {
+
         //dashboard
         Route::get('/dashboard', App\Http\Controllers\Api\Admin\DashboardController::class);
 
@@ -83,4 +73,41 @@ Route::prefix('admin')->group(function () {
         Route::apiResource('/aparaturs', App\Http\Controllers\Api\Admin\AparaturController::class)
             ->middleware('permission:aparaturs.index|aparaturs.store|aparaturs.update|aparaturs.delete');
     });
+});
+
+//group route with prefix "public"
+Route::prefix('public')->group(function () {
+
+    //index posts
+    Route::get('/posts', [App\Http\Controllers\Api\Public\PostController::class, 'index']);
+
+    //show posts
+    Route::get('/posts/{slug}', [App\Http\Controllers\Api\Public\PostController::class, 'show']);
+
+    //index posts home
+    Route::get('/posts_home', [App\Http\Controllers\Api\Public\PostController::class, 'homePage']);
+
+    //index products
+    Route::get('/products', [App\Http\Controllers\Api\Public\ProductController::class, 'index']);
+
+    //show page
+    Route::get('/products/{slug}', [App\Http\Controllers\Api\Public\ProductController::class, 'show']);
+
+    //index products home
+    Route::get('/products_home', [App\Http\Controllers\Api\Public\ProductController::class, 'homePage']);
+
+    //index pages
+    Route::get('/pages', [App\Http\Controllers\Api\Public\PageController::class, 'index']);
+
+    //show page
+    Route::get('/pages/{slug}', [App\Http\Controllers\Api\Public\PageController::class, 'show']);
+
+    //index aparaturs
+    Route::get('/aparaturs', [App\Http\Controllers\Api\Public\AparaturController::class, 'index']);
+
+    //index photos
+    Route::get('/photos', [App\Http\Controllers\Api\Public\PhotoController::class, 'index']);
+
+    //index sliders
+    Route::get('/sliders', [App\Http\Controllers\Api\Public\SliderController::class, 'index']);
 });
